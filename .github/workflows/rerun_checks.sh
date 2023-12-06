cb575670ddf36e13c306a2e2d1c910e016748013
Jonathan M Smolenski <smolensk@jpl.nasa.gov>
2023-12-06 14:51:24 -0800

    added a script to re-run pr checks based on label, updated action-remove-label to v2

---
+++ .github/workflows/rerun_checks.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+git fetch --all
+
+gh pr list --state open --label $1 --limit 500 >branches.list
+#git branch -r >branches.list
+
+#cat branches.list
+cat branches.list|grep diff\/ |sed 's|^.*diff/|diff/|g' |sed 's|\t.*||g' >branches2.list
+#cat branches.list|grep \/diff\/ |sed 's|^.*(diff/.*?)\s/.*$|$1|g' >branches2.list
+cat branches2.list                
+
+git config --global user.email ${{ github.actor }}@users.noreply.github.com
+git config --global user.name "${{ github.actor }}"
+
+while read branch; do
+  echo "Branch: $branch"
+  git checkout $branch
+  git pull origin $branch
+  echo "git commit --allow-empty -m 'Triggering Autotest Workflow Re-Run'"
+  git commit --allow-empty -m 'Triggering Autotest Workflow Re-Run'
+  echo "git push origin $branch"
+  git push origin $branch
+done <branches2.list  
