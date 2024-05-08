ca95f7f8eca26f2e1bed681f65236d7f658443e2
nlensse1 <nicholas.f.lenssen@nasa.gov>
2024-05-08 15:47:12 -0400

    remove spaces

--- tests/verify_collection.py
+++ tests/verify_collection.py
@@ -369,7 +369,6 @@ def test_spatial_subset(collection_concept_id, env, granule_json, collection_var
     # Verify spatial subset worked
     subsetted_ds = xarray.open_dataset(subsetted_filepath, decode_times=False)
     group = None
-    
     # Try to read group in file
     lat_var_name, lon_var_name = get_lat_lon_var_names(subsetted_ds, subsetted_filepath, collection_variables)
     lat_var_name = lat_var_name.split('/')[-1]
@@ -433,7 +432,6 @@ def test_spatial_subset(collection_concept_id, env, granule_json, collection_var
 
         if var_ds is None and msk is None:
             pytest.fail(f"Unable to find variable from umm-v to use as science variable.")
-
     else:
         # Can't find a science var in UMM-V, just pick one
         science_var_name = next(iter([v for v in subsetted_ds_new.variables if
