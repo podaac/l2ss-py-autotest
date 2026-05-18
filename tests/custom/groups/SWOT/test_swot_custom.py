2c6c89d476195a395ad17227417a7b7eaf0e5693
Simon Liu <simon.liu@jpl.nasa.gov>
2026-05-18 11:29:07 -0700

    removed some files

--- tests/custom/groups/SWOT/test_swot_custom.py
+++
@@ -1,9 +0,0 @@
-def test_swot_custom_smoke(collection_concept_id, granule_json):
-    if collection_concept_id not in {
-        "C1223720291-GES_DISC",
-        "C1234567890-GES_DISC"
-    }:
-        return
-
-    print(granule_json)
-    assert granule_json["meta"]["collection-concept-id"] == collection_concept_id
